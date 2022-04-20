bb7a86ce8a4c5277fdd3afcaf12e95bfd67873a1
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-04-20 14:51:16 -0700

    Add theme color selector and light/dark mode

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
+import { useTheme } from "../../hooks/useTheme";
 
 // styles
 import "./Recipe.css";
@@ -8,12 +9,24 @@ export default function Recipe() {
   const { id } = useParams();
   const url = "http://localhost:3000/recipes/" + id;
   const { error, isPending, data: recipe } = useFetch(url);
+  const { mode } = useTheme();
 
   return (
-    <div className="recipe">
+    <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {recipe && <h1>{recipe.title}</h1>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
     </div>
   );
 }
